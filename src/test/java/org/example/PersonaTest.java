e84bb08b9ecf1c64e2ee61c86064aa69b1df7b3c
FABITLER <fabinapadillarodriguez7@gmail.com>
2024-04-08 13:57:56 +0200

    calcularEdad

--- src/test/java/org/example/PersonaTest.java
+++ src/test/java/org/example/PersonaTest.java
@@ -5,9 +5,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonaTest {
+Persona p = new Persona();
 
+/*
+Probamos que la edad sea correcta
+ */
     @Test
     public void calcularEdad() {
-
+        assertEquals(34, p.getEdad());
     }
 }
