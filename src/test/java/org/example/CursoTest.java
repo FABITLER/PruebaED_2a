4240b2694bc6f3ac6cbc7406144a5783249a990d
FABITLER <fabinapadillarodriguez7@gmail.com>
2024-04-08 13:47:53 +0200

    anadirAlumno

--- src/test/java/org/example/CursoTest.java
+++ src/test/java/org/example/CursoTest.java
@@ -1,4 +1,17 @@
+package org.example;
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 class CursoTest {
-  
+Curso  c = new Curso("D1");
+ Persona pepe;
+
+/*
+  AQUI PROBAMOS QUE SE AÑADE CORRECTAMENTE LA PERSONA
+    */
+ @Test
+public void anadirAlumno() {
+  pepe = new Persona();
+   c.aniadirAlumno(pepe);
+}
 }
--- src/test/java/org/example/PersonaTest.java
+++ src/test/java/org/example/PersonaTest.java
@@ -1,4 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PersonaTest {
-  
+
+    @Test
+    public void calcularEdad() {
+
+    }
 }
