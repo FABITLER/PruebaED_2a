d872b1051c69686656c742a0eb54dbace1b1ca90
FABITLER <fabinapadillarodriguez7@gmail.com>
2024-04-09 09:14:20 +0200

    Documentada la clase ANADIARaLUMNO2

--- src/test/java/org/example/CursoTest.java
+++ src/test/java/org/example/CursoTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CursoTest {
 Curso  c = new Curso("D1");
  Persona pepe;
+ Persona pepa;
 
     /**
      * REVISAMOS QUE EL AÑADIR FUNCIONA CORRECTAMENTE REVISANOD QUE AL AÑADIR
@@ -14,8 +15,19 @@ Curso  c = new Curso("D1");
  @Test
 public void anadirAlumno() {
 pepe = new Persona();
-   c.aniadirAlumno(pepe);
+     c.aniadirAlumno(pepe);
      String resultado = c.toString();
      Assert.assertTrue(resultado.contains(pepe.getNombre()));
-}
+ }
+
+    /**
+     * revisamos ahora si se añade correctament epero con una persona con datos especificos
+     */
+    @Test
+    public void anadirAlumno2() {
+        pepa = new Persona(1223423,"Pepa", 'F',1,1,1990);
+        String resultado = c.toString();
+        c.aniadirAlumno(pepa);
+        Assert.assertTrue(resultado.contains(pepa.getNombre()));
+    }
 }
